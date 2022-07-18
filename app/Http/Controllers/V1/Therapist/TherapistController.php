ea6c715956ac0bb1606dc2a99711f0eb81598195
lamptechs <105203626+lamptechs@users.noreply.github.com>
2022-07-18 21:38:35 +0600

    Update TherapistController.php
    Therapist ID Search

--- app/Http/Controllers/V1/Therapist/TherapistController.php
+++ app/Http/Controllers/V1/Therapist/TherapistController.php
@@ -197,7 +197,7 @@ class TherapistController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Request $request)
     {
         //
         try{
